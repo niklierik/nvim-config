c1de36d69a7781f534546f2601046aff0645ce01
niklierik <erik200203@gmail.com>
2025-06-07 12:35:29 +0200

    Git force push

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<leader>ga", "<CMD>Git add .<CR>", { desc = "Git add", noremap = true })
 vim.keymap.set("n", "<leader>gc", "<CMD>Git commit<CR>", { desc = "Git commit", noremap = true })
 vim.keymap.set("n", "<leader>gp", "<CMD>Git push<CR>", { desc = "Git push", noremap = true })
+vim.keymap.set("n", "<leader>gfp", "<CMD>Git push -f<CR>", { desc = "Git force push", noremap = true })
 
 return {
 	"tpope/vim-fugitive",
