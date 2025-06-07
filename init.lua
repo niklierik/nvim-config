cf806cd3d83d795b70bd7b5f2a597d5eb25f920b
niklierik <erik200203@gmail.com>
2025-06-07 03:43:42 +0200

    Install formatter

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
-require 'core.options'
-require 'core.keymaps'
+require("core.options")
+require("core.keymaps")
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 
 ---@type vim.Option
@@ -25,13 +25,14 @@ rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
-    require("plugins.treesitter"),
-    require("plugins.bufferline"),
-    require("plugins.lualine"),
-    require("plugins.telescope"),
-    require("plugins.github-nvim-theme"),
-    require("plugins.nvim-tree"),
-    require("plugins.lsp"),
-    require("plugins.autocomplete"),
+require("lazy").setup({
+	require("plugins.treesitter"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.telescope"),
+	require("plugins.github-nvim-theme"),
+	require("plugins.nvim-tree"),
+	require("plugins.lsp"),
+	require("plugins.autocomplete"),
+	require("plugins.none-lsp"),
 })
