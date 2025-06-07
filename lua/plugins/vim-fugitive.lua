e28606e3c7dd078a83d796ed7c571fb81a9317ed
niklierik <erik200203@gmail.com>
2025-06-07 12:24:51 +0200

    Add keybindings for git commands

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,3 +1,7 @@
+vim.keymap.set("n", "<leader>ga", "<CMD>Git add .<CR>", { desc = "Git add", noremap = true })
+vim.keymap.set("n", "<leader>gc", "<CMD>Git commit<CR>", { desc = "Git add", noremap = true })
+vim.keymap.set("n", "<leader>gp", "<CMD>Git push<CR>", { desc = "Git add", noremap = true })
+
 return {
 	"tpope/vim-fugitive",
 }
