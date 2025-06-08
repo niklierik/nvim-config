b55eb862b0a7ae41766c74550a620676567140ad
niklierik <erik200203@gmail.com>
2025-06-08 22:24:01 +0200

    Add git status command

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -2,6 +2,7 @@ vim.keymap.set("n", "<leader>ga", "<CMD>Git add .<CR>", { desc = "Git add", nore
 vim.keymap.set("n", "<leader>gc", "<CMD>Git commit<CR>", { desc = "Git commit", noremap = true })
 vim.keymap.set("n", "<leader>gp", "<CMD>Git push<CR>", { desc = "Git push", noremap = true })
 vim.keymap.set("n", "<leader>gfp", "<CMD>Git push -f<CR>", { desc = "Git force push", noremap = true })
+vim.keymap.set("n", "<leader>gs", "<CMD>Git status<CR>", { desc = "Git status", noremap = true })
 
 return {
 	"tpope/vim-fugitive",
