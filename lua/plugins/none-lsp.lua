eb5ed0fd11333f4a5b0eb4082162f2458ba2e887
niklierik <erik200203@gmail.com>
2025-06-10 20:56:11 +0200

    Add JSON config for LSP

--- lua/plugins/none-lsp.lua
+++ lua/plugins/none-lsp.lua
@@ -25,7 +25,7 @@ return {
     local sources = {
       diagnostics.checkmake,
       formatting.prettier.with({
-        filetypes = { "html", "json", "yaml", "markdown", "typescript", "javascript", "scss" },
+        filetypes = { "html", "json", "jsonc", "yaml", "markdown", "typescript", "javascript", "scss" },
       }),
       formatting.stylua,
       formatting.shfmt.with({ args = { "-i", "4" } }),
