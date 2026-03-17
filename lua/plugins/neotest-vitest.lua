34979f834b7612f7a78809d3959b99c57683a345
niklierik <erik200203@gmail.com>
2026-03-17 10:00:28 +0100

    asd

--- lua/plugins/neotest-vitest.lua
+++ lua/plugins/neotest-vitest.lua
@@ -9,4 +9,19 @@ return {
 			["neotest-vitest"] = {},
 		},
 	},
+	config = function()
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>Twr",
+			"<cmd>lua require('neotest').run.run({ vitestCommand = 'vitest --watch' })<cr>",
+			{ desc = "Run Watch" }
+		)
+
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>Twf",
+			"<cmd>lua require('neotest').run.run({ vim.fn.expand('%'), vitestCommand = 'vitest --watch' })<cr>",
+			{ desc = "Run Watch File" }
+		)
+	end,
 }
